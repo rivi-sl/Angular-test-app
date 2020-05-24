57abe46790a7187e9969786d5b0727572c22effd
STEdirisuriya <supunedirisuriya2002@gmail.com>
2020-05-24 07:31:49 +0530

    changed env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC9i84WPf-BThhQA-Ygh3_07OuFtvo3gt0",
+    authDomain: "rivi-k.firebaseapp.com",
+    databaseURL: "https://rivi-k.firebaseio.com",
+    projectId: "rivi-k",
+    storageBucket: "rivi-k.appspot.com",
+    messagingSenderId: "845158057150",
+    appId: "1:845158057150:web:71f8e3721797f4fdd94aef",
+    measurementId: "G-B2Z8C8MZ06"
+  }
 };
